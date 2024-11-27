b7bf36df170c9cc530599588a4aabc39d3069de7
Samuel Hoyas <samuelix97@gmail.com>
2024-11-27 19:06:05 +0100

    Header component

--- vite.config.ts
+++ vite.config.ts
@@ -13,12 +13,19 @@ export default defineConfig({
     postcss: {
       plugins: [autoprefixer],
     },
+    preprocessorOptions: {
+      scss: {
+        includePaths: [path.resolve(__dirname, 'src/assets/styles')],
+      },
+    },
   },
   resolve: {
     alias: {
       '@': path.resolve(__dirname, './src'),
       '@state': path.resolve(__dirname, './src/state'),
       '@components': path.resolve(__dirname, './src/components'),
+      '@elements': path.resolve(__dirname, './src/elements'),
+      '@assets': path.resolve(__dirname, './src/assets'),
     },
   },
 });
